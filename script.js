acea287e59096d4b07bee9764c469bfe63e536a2
bemail2017 <140424700+bemail2017@users.noreply.github.com>
2023-08-03 09:55:03 +0700

    Update script.js

--- script.js
+++ script.js
@@ -8,8 +8,10 @@ async function updateCryptoPrices() {
         const row = tableBody.insertRow();
         const nameCell = row.insertCell();
         const priceCell = row.insertCell();
+        const infoCell = row.insertCell();
         nameCell.innerText = crypto.name;
         priceCell.innerText = `$${crypto.price}`;
+        infoCell.innerHTML = `<a href="${crypto.infoUrl}" class="link" target="_blank">Xem thêm</a>`;
     });
 }
 
@@ -17,7 +19,11 @@ async function getCryptoPrices() {
     try {
         const response = await fetch('https://api.coingecko.com/api/v3/simple/price?ids=bitcoin,ethereum,ripple,cardano,polkadot,binancecoin,litecoin,chainlink,stellar,bitcoin-cash&vs_currencies=usd');
         const data = await response.json();
-        const cryptoPrices = Object.keys(data).map(name => ({ name, price: data[name].usd }));
+        const cryptoPrices = Object.keys(data).map(name => ({
+            name,
+            price: data[name].usd,
+            infoUrl: `https://www.coingecko.com/en/coins/${name}`
+        }));
         return cryptoPrices;
     } catch (error) {
         console.error('Lỗi khi lấy giá Crypto:', error);
