7fa7fbd8379de11250610b0969043b3b0ffc65d3
bemail2017 <140424700+bemail2017@users.noreply.github.com>
2023-08-03 10:16:21 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,43 +1,49 @@
-async function updateCryptoPrices() {
-    const cryptoData = await getCryptoPrices();
-    const currentTime = new Date().toLocaleTimeString();
-    const tableBody = document.querySelector('tbody');
-    tableBody.innerHTML = ''; // Clear previous data
+async function updateCryptoTable() {
+    const cryptoData = await getCryptoData();
+    const cryptoTableBody = document.getElementById('cryptoTableBody');
+    cryptoTableBody.innerHTML = ''; // Clear the table body
 
-    let rank = 1;
-    for (const crypto of cryptoData) {
-        const row = `
-            <tr>
-                <td>${rank}</td>
-                <td>${crypto.name}</td>
-                <td>$${crypto.price}</td>
-                <td><a href="${crypto.web}" target="_blank">Xem thêm</a></td>
-            </tr>
-        `;
-        tableBody.insertAdjacentHTML('beforeend', row);
-        rank++;
+    for (let i = 0; i < cryptoData.length; i++) {
+        const row = document.createElement('tr');
+        const rankCell = document.createElement('td');
+        rankCell.textContent = cryptoData[i].rank;
+        const nameCell = document.createElement('td');
+        nameCell.textContent = cryptoData[i].name;
+        const priceCell = document.createElement('td');
+        priceCell.textContent = `$${cryptoData[i].price}`;
+        const webCell = document.createElement('td');
+        const webLink = document.createElement('a');
+        webLink.textContent = 'Xem thêm';
+        webLink.href = cryptoData[i].website;
+        webLink.target = '_blank';
+        webCell.appendChild(webLink);
+
+        row.appendChild(rankCell);
+        row.appendChild(nameCell);
+        row.appendChild(priceCell);
+        row.appendChild(webCell);
+        cryptoTableBody.appendChild(row);
     }
-
-    const timeCell = document.getElementById('time');
-    timeCell.innerText = currentTime;
 }
 
-async function getCryptoPrices() {
+async function getCryptoData() {
     try {
         const response = await fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1');
         const data = await response.json();
-        const cryptoData = data.map(crypto => {
-            return {
-                name: crypto.name,
-                price: crypto.current_price,
-                web: crypto.links.homepage[0]
-            };
-        });
-        return cryptoData;
+        return data.map(item => ({
+            rank: item.market_cap_rank,
+            name: item.name,
+            price: item.current_price,
+            website: item.links.homepage[0]
+        }));
     } catch (error) {
         console.error('Lỗi khi lấy dữ liệu:', error);
         return [];
     }
 }
 
-setInterval(updateCryptoPrices, 30000);
+// Update table every 30 seconds
+setInterval(updateCryptoTable, 30000);
+
+// Initial update when page loads
+updateCryptoTable();
