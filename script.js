a691021235cef6604a06413485f4ab7b6d22f7a4
bemail2017 <140424700+bemail2017@users.noreply.github.com>
2023-08-03 10:11:28 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,42 +1,43 @@
-body {
-    font-family: Arial, sans-serif;
-    text-align: center;
-    margin: 20px;
+async function updateCryptoPrices() {
+    const cryptoData = await getCryptoPrices();
+    const currentTime = new Date().toLocaleTimeString();
+    const tableBody = document.querySelector('tbody');
+    tableBody.innerHTML = ''; // Clear previous data
+
+    let rank = 1;
+    for (const crypto of cryptoData) {
+        const row = `
+            <tr>
+                <td>${rank}</td>
+                <td>${crypto.name}</td>
+                <td>$${crypto.price}</td>
+                <td><a href="${crypto.web}" target="_blank">Xem thêm</a></td>
+            </tr>
+        `;
+        tableBody.insertAdjacentHTML('beforeend', row);
+        rank++;
+    }
+
+    const timeCell = document.getElementById('time');
+    timeCell.innerText = currentTime;
 }
 
-h1 {
-    color: #333;
+async function getCryptoPrices() {
+    try {
+        const response = await fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1');
+        const data = await response.json();
+        const cryptoData = data.map(crypto => {
+            return {
+                name: crypto.name,
+                price: crypto.current_price,
+                web: crypto.links.homepage[0]
+            };
+        });
+        return cryptoData;
+    } catch (error) {
+        console.error('Lỗi khi lấy dữ liệu:', error);
+        return [];
+    }
 }
 
-table {
-    border-collapse: collapse;
-    width: 100%;
-    max-width: 600px;
-    margin: 20px auto;
-    background-color: #f0f0f0;
-    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
-}
-
-th, td {
-    padding: 10px;
-    text-align: left;
-    border-bottom: 1px solid #ddd;
-}
-
-th {
-    background-color: #333;
-    color: white;
-}
-
-tr:hover {
-    background-color: #f5f5f5;
-}
-
-a {
-    color: blue;
-    text-decoration: none;
-}
-
-a:hover {
-    text-decoration: underline;
-}
+setInterval(updateCryptoPrices, 30000);
