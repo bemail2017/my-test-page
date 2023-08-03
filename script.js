52a79ef1abe3ea5715a8fa62d556167b811d88ec
bemail2017 <140424700+bemail2017@users.noreply.github.com>
2023-08-03 10:10:50 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,38 +1,42 @@
-// script.js
-async function updateCryptoPrices() {
-    const cryptoPrices = await getCryptoPrices();
-    const tableBody = document.getElementById('cryptoTableBody');
-    tableBody.innerHTML = '';
-
-    cryptoPrices.forEach((crypto, index) => {
-        const row = tableBody.insertRow();
-        const rankCell = row.insertCell();
-        const nameCell = row.insertCell();
-        const priceCell = row.insertCell();
-        const webCell = row.insertCell();
-
-        rankCell.innerText = index + 1;
-        nameCell.innerText = crypto.name;
-        priceCell.innerText = `$${crypto.price}`;
-        webCell.innerHTML = `<a href="${crypto.web}" class="link" target="_blank">Xem thêm</a>`;
-    });
+body {
+    font-family: Arial, sans-serif;
+    text-align: center;
+    margin: 20px;
 }
 
-async function getCryptoPrices() {
-    try {
-        const response = await fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1');
-        const data = await response.json();
-        const cryptoPrices = data.map(coin => ({
-            name: coin.name,
-            price: coin.current_price,
-            web: coin.links.homepage[0]
-        }));
-        return cryptoPrices;
-    } catch (error) {
-        console.error('Lỗi khi lấy giá Crypto:', error);
-        return [];
-    }
+h1 {
+    color: #333;
 }
 
-updateCryptoPrices();
-setInterval(updateCryptoPrices, 30000); // Cập nhật giá mỗi 30 giây
+table {
+    border-collapse: collapse;
+    width: 100%;
+    max-width: 600px;
+    margin: 20px auto;
+    background-color: #f0f0f0;
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+}
+
+th, td {
+    padding: 10px;
+    text-align: left;
+    border-bottom: 1px solid #ddd;
+}
+
+th {
+    background-color: #333;
+    color: white;
+}
+
+tr:hover {
+    background-color: #f5f5f5;
+}
+
+a {
+    color: blue;
+    text-decoration: none;
+}
+
+a:hover {
+    text-decoration: underline;
+}
