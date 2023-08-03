2df843bcb4170bbb174f84682c4b3f5f94264b30
bemail2017 <140424700+bemail2017@users.noreply.github.com>
2023-08-03 09:51:15 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,29 @@
-async function updateBTCPrice() {
-    const btcPrice = await getBTCPrice();
-    const currentTime = new Date().toLocaleTimeString();
-    document.getElementById('btcPrice').innerText = `$${btcPrice}`;
-    document.getElementById('time').innerText = currentTime;
+// script.js
+async function updateCryptoPrices() {
+    const cryptoPrices = await getCryptoPrices();
+    const tableBody = document.getElementById('cryptoTableBody');
+    tableBody.innerHTML = '';
+
+    cryptoPrices.forEach(crypto => {
+        const row = tableBody.insertRow();
+        const nameCell = row.insertCell();
+        const priceCell = row.insertCell();
+        nameCell.innerText = crypto.name;
+        priceCell.innerText = `$${crypto.price}`;
+    });
 }
 
-async function getBTCPrice() {
+async function getCryptoPrices() {
     try {
-        const response = await fetch('https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd');
+        const response = await fetch('https://api.coingecko.com/api/v3/simple/price?ids=bitcoin,ethereum,ripple,cardano,polkadot,binancecoin,litecoin,chainlink,stellar,bitcoin-cash&vs_currencies=usd');
         const data = await response.json();
-        const btcPrice = data.bitcoin.usd;
-        return btcPrice;
+        const cryptoPrices = Object.keys(data).map(name => ({ name, price: data[name].usd }));
+        return cryptoPrices;
     } catch (error) {
-        console.error('Lỗi khi lấy giá BTC:', error);
-        return 'Không thể lấy giá.';
+        console.error('Lỗi khi lấy giá Crypto:', error);
+        return [];
     }
 }
 
-setInterval(updateBTCPrice, 30000);
+updateCryptoPrices();
+setInterval(updateCryptoPrices, 30000); // Cập nhật giá mỗi 30 giây
